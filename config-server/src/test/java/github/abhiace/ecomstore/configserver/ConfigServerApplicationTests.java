a14d927ef9970c9ab663c0e15191f2ffe1b11a10
Abhijit <abhijtnayak567@gmail.com>
2023-08-19 23:56:32 +0530

    add config server

---
+++ config-server/src/test/java/github/abhiace/ecomstore/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.abhiace.ecomstore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
